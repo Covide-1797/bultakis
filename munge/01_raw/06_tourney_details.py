ec3c093ed14598d0e6815e92bcb5de27abade982
luqman.bachtiar <luqman.bachtiar@fonterra.com>
2018-12-01 18:17:54 +1300

    Refactor get tourney details

--- munge/01_raw/06_tourney_details.py
+++ munge/01_raw/06_tourney_details.py
@@ -2,14 +2,93 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
-import time
+#import time
 
 def get_soup(url_link):
     req = Request(url_link, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     soup_html = BeautifulSoup(webpage, 'html.parser')    
-    time.sleep(15) #sleep for 3 seconds
+#    time.sleep(15) #sleep for 3 seconds
     return soup_html
+
+def game_score_array_single(game_score, match_soup):
+    # Player 1 id
+    p1_chunk = match_soup.findAll('div', {'class':'player1-name'})
+    url_p1 = re.search('href="(.*)">', str(p1_chunk)).group(1)
+    p1_split = url_p1.split('/')
+    p1_pid = p1_split[4]
+
+    # Player 2 id
+    p2_chunk = match_soup.findAll('div', {'class':'player2-name'})
+    url_p2 = re.search('href="(.*)">', str(p2_chunk)).group(1)
+    p2_split = url_p2.split('/')
+    p2_pid = p2_split[4]
+    
+    game_det = match_soup.findAll('div', {'class':'game-completed-wrap'})        
+    games_played = len(game_det)
+    for c, games in enumerate(game_det, 1):
+        games_split = games.find_all('div')
+        p1_score = games_split[2].text
+        p1_score = p1_score.replace("\t","").replace("\n","")
+        p2_score = games_split[4].text
+        p2_score = p2_score.replace("\t","").replace("\n","")
+        scores = {
+                't_id': tourney_id,
+                't_date': tourney_date,
+                'games_played': games_played,
+                'p1a_pid': p1_pid,
+                'p1b_pid': '',
+                'p2a_pid': p2_pid,
+                'p2b_pid': '',
+                'game': c,
+                'p1_score': p1_score,
+                'p2_score': p2_score}
+#                    print(scores)
+        game_score.append(scores)   
+    return game_score
+
+def game_score_array_double(game_score, match_soup):
+    # Player 1 id                
+    p1_chunk = match_soup.findAll('div', {'class':'player1-name'})                
+    url_p1a = re.findall('href="(.*)">', str(p1_chunk))[0]
+    url_p1b = re.findall('href="(.*)">', str(p1_chunk))[1]                
+    p1_split = url_p1a.split('/')
+    p1a_pid = p1_split[4]                
+    p1_split = url_p1b.split('/')
+    p1b_pid = p1_split[4]
+    
+    
+    # Player 1 id                
+    p2_chunk = match_soup.findAll('div', {'class':'player2-name'})                
+    url_p2a = re.findall('href="(.*)">', str(p2_chunk))[0]
+    url_p2b = re.findall('href="(.*)">', str(p2_chunk))[1]                
+    p2_split = url_p2a.split('/')
+    p2a_pid = p2_split[4]                
+    p2_split = url_p2b.split('/')
+    p2b_pid = p2_split[4]
+                  
+    game_det = match_soup.findAll('div', {'class':'game-completed-wrap'})        
+    games_played = len(game_det)
+    for c, games in enumerate(game_det, 1):
+        games_split = games.find_all('div')
+        p1_score = games_split[2].text
+        p1_score = p1_score.replace("\t","").replace("\n","")
+        p2_score = games_split[4].text
+        p2_score = p2_score.replace("\t","").replace("\n","")
+        scores = {
+                't_id': tourney_id,
+                't_date': tourney_date,
+                'games_played': games_played,
+                'p1a_pid': p1a_pid,
+                'p1b_pid': p1b_pid,
+                'p2a_pid': p2a_pid,
+                'p2b_pid': p2b_pid,
+                'game': c,
+                'p1_score': p1_score,
+                'p2_score': p2_score}
+        print(scores)
+        game_score.append(scores)   
+    return game_score
 
 #game_score = []
 # Load tourney list
@@ -17,7 +96,7 @@ tourney_list = pd.read_csv('../../data/01_raw/tournament_year.csv', sep="|")
 
 for t_step in range(0, len(tourney_list)):
     game_score = []
-    print("Tourney " + str(t_step) + " of " + str(len(tourney_list)))
+    print("Tourney " + str(t_step+1) + " of " + str(len(tourney_list)))
 #for t_step in range(0, 2):
     url_hit = tourney_list['t_web_link'][t_step]
     print(url_hit)
@@ -46,43 +125,23 @@ for t_step in range(0, len(tourney_list)):
                 print("Game " + str(game_count) + " of " + str(len(game_table_split)/2))
                 game_count+=1
                 get_id = str(game_table_split[get_game_details])
-                url_match = re.search('href="(.*)" id', get_id).group(1)
-                url_match = url_match.replace("&amp;", "&")        
-                match_soup = get_soup(url_match)
-                
-                get_date = url_match.split('/')
-                tourney_date = get_date[6]
-                # Player 1 id
-                p1_chunk = match_soup.findAll('div', {'class':'player1-name'})
-                url_p1 = re.search('href="(.*)">', str(p1_chunk)).group(1)
-                p1_split = url_p1.split('/')
-                p1_pid = p1_split[4]
-        
-                # Player 2 id
-                p2_chunk = match_soup.findAll('div', {'class':'player2-name'})
-                url_p2 = re.search('href="(.*)">', str(p2_chunk)).group(1)
-                p2_split = url_p2.split('/')
-                p2_pid = p2_split[4]
-                
-                game_det = match_soup.findAll('div', {'class':'game-completed-wrap'})        
-                games_played = len(game_det)
-                for c, games in enumerate(game_det, 1):
-                    games_split = games.find_all('div')
-                    p1_score = games_split[2].text
-                    p1_score = p1_score.replace("\t","").replace("\n","")
-                    p2_score = games_split[4].text
-                    p2_score = p2_score.replace("\t","").replace("\n","")
-                    scores = {
-                            't_id': tourney_id,
-                            't_date': tourney_date,
-                            'games_played': games_played,
-                            'p1_pid': p1_pid,
-                            'p2_pid': p2_pid,
-                            'game': c,
-                            'p1_score': p1_score,
-                            'p2_score': p2_score}
-#                    print(scores)
-                    game_score.append(scores)   
+                if re.search('href="(.*)">', get_id) is not None:   
+                    url_match = re.search('href="(.*)" id', get_id).group(1)
+                    url_match = url_match.replace("&amp;", "&")        
+                    match_soup = get_soup(url_match)
+                    
+                    get_date = url_match.split('/')
+                    tourney_date = get_date[6]
+                    
+                    check_match_type = match_soup.findAll('div', {'class':'player1-name'})
+                    
+                    if len(check_match_type) == 2:
+                        match_type='doubles'
+                        game_score = game_score_array_double(game_score, match_soup)
+                    else:
+                        match_type='singles'
+                        game_score = game_score_array_single(game_score, match_soup)                    
+
         tourney_scores_df = pd.DataFrame(game_score)  
         filename = "../../data/01_raw/tourney_details_{}.csv".format(tourney_id)
         tourney_scores_df.to_csv(filename, index=False, sep='|', header=True)
